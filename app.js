fcb211bc0b2e150d9db070f65e968b72ce97f888
Yatish Chandra Pitta <ycchandra29@gmail.com>
2020-07-27 16:08:56 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req,res){
+    res.render("home");
+});
+
+app.get("/about", function(req, res){
+    res.render("about");
+});
+
+app.listen(3000, function(err){
+    if(err){
+        console.log('Error has occured while starting the server');
+    }
+})
