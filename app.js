1c83eff72c2625d916926fe709a31bf193a2eb30
Yatish Chandra Pitta <ycchandra29@gmail.com>
2020-07-27 22:23:14 -0400

    added template for sections

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 var express = require('express');
 var app = express();
 
-app.set("view engine", "ejs");
+var port = process.env.PORT || 3000;
 
+app.set("view engine", "ejs");
 app.get("/", function(req,res){
     res.render("home");
 });
 
-app.get("/about", function(req, res){
-    res.render("about");
+app.get("/wellness", function(req, res){
+    res.render("wellness");
 });
 
-app.listen(process.env.PORT, function(err){
+app.get("/education", function(req, res){
+    res.render("education");
+});
+
+app.get("/finance", function(req, res){
+    res.render("finance");
+});
+
+app.listen(port, function(err){
     if(err){
         console.log('Error has occured while starting the server');
+    }
+    else{
+        console.log('Started listening on port' + port);
     }
 })
