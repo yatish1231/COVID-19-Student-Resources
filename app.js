658e601b7a86a232200401b82d94ded216109757
Yatish Chandra Pitta <ycchandra29@gmail.com>
2020-08-19 17:08:51 -0400

    Home page done

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
+app.use(express.static(__dirname+"/public"))
 app.get("/", function(req,res){
     res.render("home");
 });
